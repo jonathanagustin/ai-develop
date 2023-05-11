d7be2922fafe15042d774b50f4f3f0471dc54bd3
jonathanagustin <jonathanagustin@gmail.com>
2023-05-11 05:43:13 +0000

    save

--- .devcontainer/postattach.sh
+++ .devcontainer/postattach.sh
@@ -2,35 +2,13 @@
 # Executes when connecting to Codespace (through browser or VSCode client)
 
 install_extensions() {
-    # fix: install here to ensure installed after ms-python.python
+    # install extension here or else must reload
     code --force --install-extension ms-python.vscode-pylance
 }
 
-install_conda() {
-    # download and install miniconda
-    curl -sSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o ~/miniconda.sh
-    chmod +x ~/miniconda.sh
-    ~/miniconda.sh -b -u -p ~/.miniconda
-    rm -rf ~/miniconda.sh
-
-    # add conda to PATH if not already in path
-    if ! grep -qxF "export PATH=\$PATH:${HOME}/.miniconda/bin" ~/.bashrc; then
-        echo "export PATH=\$PATH:${HOME}/.miniconda/bin" >> ~/.bashrc
-    fi
-    source ~/.bashrc
-
-    # add conda to PATH for this session
-    export PATH="\$PATH:${HOME}/.miniconda/bin"
-
-    # configure conda
-    conda config --set auto_activate_base false
-    conda config --add channels conda-forge
-    conda init bash
-}
-
-install_env() {
-    conda create -n default -y
-    conda install -y \
+install_conda_packages() {
+    # install base packages
+    /home/vscode/.miniconda/bin/conda install -y \
         jupyter \
         ipykernel \
         notebook \
@@ -57,9 +35,8 @@ install_env() {
 
 main() {
     install_extensions &
-    install_conda
-    install_env
+    install_conda_packages &
 }
 
 # output to log
-main > .devcontainer/postattach.log 2>&1
+main > /tmp/postattach.log 2>&1
--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,19 +1,51 @@
 #!/usr/bin/env bash
-# Executes after the creating Codespace and cloning source code
+# Executes after the creating Codespace and cloning repo
 
-update_pip() {
+upgrade_pip() {
     python -m pip install --upgrade pip
 }
 
 install_venv() {
+    # create virtual environment
     python -m venv .venv
-    .venv/bin/pip install -r requirements.txt
+
+    # install packages
+    /workspaces/ai-develop/.venv/bin/pip install -r /workspaces/ai-develop/requirements.txt
+}
+
+install_conda() {
+    # download and install miniconda
+    curl -sSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o ~/miniconda.sh
+    chmod +x ~/miniconda.sh
+    ~/miniconda.sh -b -u -p ~/.miniconda
+    rm -rf ~/miniconda.sh
+
+    # add conda to PATH if not already in path
+    LINE="export PATH=\$PATH:$HOME/.miniconda/bin"
+    if ! /bin/grep -qxF $LINE /home/vscode/.bashrc; then
+        echo "$LINE" >> /home/vscode/.bashrc
+    fi
+
+    # add conda to PATH for this session
+    export PATH="\$PATH:$HOME/.miniconda/bin"
+
+    # configure conda
+    conda config --set auto_activate_base false
+    conda config --add channels conda-forge
+
+    # fix: activate venv on shell start must be after conda init
+    conda init bash
+    LINE="source /workspaces/ai-develop/.venv/bin/activate"
+    if ! /bin/grep -qxF "$LINE" /home/vscode/.bashrc; then
+        echo "$LINE" >> /home/vscode/.bashrc
+    fi
 }
 
 main() {
-    update_pip &
-    install_venv &
+    upgrade_pip
+    install_venv
+    install_conda
 }
 
 # output to log
-main > .devcontainer/postcreate.log 2>&1
+main > /tmp/postcreate.log 2>&1
