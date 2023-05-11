b2763b319597cce5b58389fed3db535459f12977
jonathanagustin <jonathanagustin@gmail.com>
2023-05-11 01:12:24 +0000

    save

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -2,33 +2,43 @@
 
 # Executes after the creating Codespace and cloning source code
 
-install_python_libs() {
-conda install mamba -n base -c conda-forge
-
-mamba install -y \
-    ipykernel \
-    jupyter \
-    notebook \
-    pandoc \
-    nbconvert \
-    autopep8 \
-    pandas \
-    matplotlib \
-    seaborn \
-    numpy \
-    scipy \
-    scikit-learn \
-    fastai \
-    gensim \
-    huggingface \
-    keras \
-    nltk \
-    opencv \
-    pytorch \
-    spacy \
-    tensorflow \
-    torchvision \
-    transformers
+install_mamba() {
+    curl -sSL "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh" -o /tmp/mamba.sh
+    sudo chmod +x /tmp/mamba.sh
+    bash /tmp/mamba.sh -b -u
 }
 
-install_python_libs > /dev/null 2>&1 &
+install_default_env() {
+    /home/vscode/.local/bin/micromamba install -y \
+        ipykernel \
+        jupyter \
+        notebook \
+        pandoc \
+        nbconvert \
+        autopep8 \
+        pandas \
+        matplotlib \
+        seaborn \
+        numpy \
+        scipy \
+        scikit-learn \
+        fastai \
+        gensim \
+        huggingface \
+        keras \
+        nltk \
+        opencv \
+        pytorch \
+        spacy \
+        tensorflow \
+        torchvision \
+        transformers \
+        -c conda-forge
+}
+
+main() {
+    install_mamba
+    install_default_env
+}
+
+main
