6247e7b5b70fa96d7025e4f2a81ab7c7d453be64
jonathanagustin <jonathanagustin@gmail.com>
2023-05-12 01:09:36 +0000

    test

--- .devcontainer/oncreate.sh
+++ .devcontainer/oncreate.sh
@@ -2,14 +2,18 @@
 # Executes after the creating Codespace and cloning repo
 
 get_info() {
-    echo "-----------------------------"
-    echo "Date: $(date)"
-    echo "User: $(whoami)"
-    echo "Home: $HOME"
-    echo "Shell: $SHELL"
-    echo "Working Directory: $(pwd)"
-    echo "PATH: $PATH"
-    echo "-----------------------------"
+    echo -e "-----------------------------"
+    echo -e "Date: $(date)"
+    echo -e "User: $(whoami)"
+    echo -e "Home: $HOME"
+    echo -e "Shell: $SHELL"
+    echo -e "Working Directory: $(pwd)"
+    echo -e "PATH: $PATH"
+    # test whether code is installed at this point
+    echo -e "code: $(which code)"
+    # test what directories are available at $HOME
+    echo -e "Directories: $(cd ~ && ls -la)"
+    echo -e "-----------------------------"
 }
 
 add_custom_welcome() {
@@ -24,24 +28,65 @@ install_apt_libs() {
 }
 
 install_conda() {
+
     # download and install miniconda
-    curl -sSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o /opt/miniconda.sh
-    chmod +x /opt/miniconda.sh
-    bash -c /opt/miniconda.sh -b -u -p /opt/.miniconda
-    rm -rf /opt/miniconda.sh
+    curl -sSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o "${HOME}/miniconda.sh"
+    chmod +x "${HOME}/miniconda.sh"
+
+    # -b = batch mode (no prompts)
+    # -u = unattended mode
+    # -p = prefix for install location
+    bash -c "${HOME}/miniconda.sh" -b -u -p "${HOME}/.miniconda"
+
+    # clean up
+    rm -rf "${HOME}/miniconda.sh"
+
+    # add conda to PATH
+    if ! /bin/grep -qxF "export PATH=\$PATH:${HOME}/.miniconda/bin" /home/vscode/.bashrc; then
+        echo "export PATH=\$PATH:${HOME}/.miniconda/bin" >> /home/vscode/.bashrc
+    fi
 
     # configure conda
-    /opt/.miniconda/bin/conda config --set auto_activate_base false
-    /opt/.miniconda/bin/conda config --add channels conda-forge
-    /opt/.miniconda/bin/conda init bash
+    "${HOME}/.miniconda/bin/conda" config --add channels conda-forge
+
+    # initialize conda
+    "${HOME}/.miniconda/bin/conda" init bash
+
+    # create default environment
+    "${HOME}/.miniconda/bin/conda" create -y -n default python=3.8
+
+    # install packages into default environment
+    "${HOME}/.miniconda/bin/conda" install -n default -y \
+        jupyter \
+        ipykernel \
+        notebook \
+        pandoc \
+        nbconvert \
+        pandas \
+        matplotlib \
+        seaborn \
+        numpy \
+        scipy \
+        scikit-learn \
+        fastai \
+        gensim \
+        huggingface_hub \
+        keras \
+        nltk \
+        opencv \
+        pytorch \
+        spacy \
+        tensorflow \
+        torchvision \
+        transformers
 }
 
 main() {
-    get_info &
+    get_info
     add_custom_welcome &
-    install_apt_libs
+    install_apt_libs &
+    install_conda
     wait
 }
 
-# output to log
 main
--- .devcontainer/postattach.sh
+++ .devcontainer/postattach.sh
@@ -1,39 +1,8 @@
 #!/usr/bin/env bash
 # Executes when connecting to Codespace (through browser or VSCode client)
 
-install_pip_packages() {
-    /workspaces/ai-develop/.venv/bin/pip install -r /workspaces/ai-develop/requirements.txt
-}
-
-install_conda_packages() {
-    /home/vscode/.miniconda/bin/conda install -y \
-        jupyter \
-        ipykernel \
-        notebook \
-        pandoc \
-        nbconvert \
-        pandas \
-        matplotlib \
-        seaborn \
-        numpy \
-        scipy \
-        scikit-learn \
-        fastai \
-        gensim \
-        huggingface_hub \
-        keras \
-        nltk \
-        opencv \
-        pytorch \
-        spacy \
-        tensorflow \
-        torchvision \
-        transformers
-}
-
 main() {
-    install_pip_packages &
-    install_conda_packages &
+
 }
 
 # output to log
