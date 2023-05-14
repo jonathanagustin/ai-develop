15cfee781c37cec80897e821059c94448a1049f9
jonathanagustin <jonathanagustin@gmail.com>
2023-05-13 22:51:30 -0400

    save

--- .devcontainer/oncreate.sh
+++ .devcontainer/oncreate.sh
@@ -20,73 +20,9 @@ add_custom_welcome() {
     sudo cp .devcontainer/first-run-notice.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
 }
 
-install_apt_libs() {
-    sudo apt-get update
-
-    # required for converting notebooks to PDF
-    sudo apt-get install -y texlive-xetex \
-        texlive-fonts-recommended \
-        texlive-plain-generic
-}
-
-install_conda() {
-    # download and install miniconda
-    curl -sSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o "${HOME}/miniconda.sh"
-    chmod +x "${HOME}/miniconda.sh"
-
-    # -b = batch mode (no prompts)
-    # -u = unattended mode
-    # -p = prefix for install location
-    bash -c "${HOME}/miniconda.sh" -b -u -p "${HOME}/.miniconda"
-
-    # clean up
-    rm -rf "${HOME}/miniconda.sh"
-
-    # add conda to PATH
-    if ! /bin/grep -qxF "export PATH=\$PATH:${HOME}/.miniconda/bin" /home/vscode/.bashrc; then
-        echo "export PATH=\$PATH:${HOME}/.miniconda/bin" >> /home/vscode/.bashrc
-    fi
-
-    # configure conda
-    "${HOME}/.miniconda/bin/conda" config --add channels conda-forge
-
-    # initialize conda
-    "${HOME}/.miniconda/bin/conda" init bash
-
-    # create default environment
-    "${HOME}/.miniconda/bin/conda" create -y -n default python=3.8
-
-    # install packages into default environment
-    "${HOME}/.miniconda/bin/conda" install -n default -y \
-        jupyter \
-        ipykernel \
-        notebook \
-        pandoc \
-        nbconvert \
-        pandas \
-        matplotlib \
-        seaborn \
-        numpy \
-        scipy \
-        scikit-learn \
-        fastai \
-        gensim \
-        huggingface_hub \
-        keras \
-        nltk \
-        opencv \
-        pytorch \
-        spacy \
-        tensorflow \
-        torchvision \
-        transformers
-}
-
 main() {
-    get_info
+    get_info &
     add_custom_welcome &
-    install_apt_libs &
-    install_conda
     wait
 }
 
--- .devcontainer/postattach.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Executes when connecting to Codespace (through browser or VSCode client)
-
-main() {
-
-}
-
-# output to log
-main > /tmp/postattach.log 2>&1
